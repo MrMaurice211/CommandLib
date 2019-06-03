b635fdd9a71756cdb936b64e6c0e2520838d5ac6
MrMaurice211 <t_ulu_m@live.com.mx>
2019-06-02 22:43:48 -0500

    First push

--- src/main/java/io/apexcreations/ApexCommand.java
+++ src/main/java/io/apexcreations/ApexCommand.java
@@ -11,82 +11,77 @@ import org.bukkit.entity.Player;
 
 public abstract class ApexCommand extends Command {
 
-    private final Map<String, ApexSubCommand> subCommandCache = new HashMap<>();
-    private final boolean playerOnly;
+	private final Map<String, ApexSubCommand> subCommandCache = new HashMap<>();
+	private final boolean playerOnly;
 
-    public ApexCommand(String name) {
-        this(name, false);
-    }
+	public ApexCommand(String name) {
+		this(name, false);
+	}
 
-    public ApexCommand(String name, boolean playerOnly) {
-        this(name, "Unknown Description", "", playerOnly);
-    }
+	public ApexCommand(String name, boolean playerOnly) {
+		this(name, "Unknown Description", "", playerOnly);
+	}
 
-    public ApexCommand(String name, String description, String permission,
-            boolean playerOnly, String... aliases) {
-        super(name);
-        this.setDescription(description);
-        this.setPermission(permission);
-        this.playerOnly = playerOnly;
-        this.setAliases(Arrays.asList(aliases));
-        this.setPermissionMessage(ChatColor.RED + "You do not have permission for this command!");
-    }
+	public ApexCommand(String name, String description, String permission, boolean playerOnly, String... aliases) {
+		super(name);
+		this.setDescription(description);
+		this.setPermission(permission);
+		this.playerOnly = playerOnly;
+		this.setAliases(Arrays.asList(aliases));
+		this.setPermissionMessage(ChatColor.RED + "You do not have permission for this command!");
+	}
 
-    @Override
-    public boolean execute(CommandSender commandSender, String label, String[] args) {
-        if (!(commandSender instanceof Player) && this.playerOnly) {
-            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    this.getPermissionMessage()));
-            return false;
-        }
-        if (!getPermission().isEmpty() && !commandSender.hasPermission(getPermission())) {
-            commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                    this.getPermissionMessage()));
-            return true;
-        }
+	@Override
+	public boolean execute(CommandSender commandSender, String label, String[] args) {
+		if (!(commandSender instanceof Player) && this.playerOnly) {
+			commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getPermissionMessage()));
+			return false;
+		}
+		if (!getPermission().isEmpty() && !commandSender.hasPermission(getPermission())) {
+			commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', this.getPermissionMessage()));
+			return true;
+		}
 
-        if (args.length > 0) {
-            Optional<ApexSubCommand> optionalSubCommand = this.getSubCommand(args[0]);
-            if (optionalSubCommand.isPresent()) {
-                ApexSubCommand subCommand = optionalSubCommand.get();
-                if (!commandSender.hasPermission(subCommand.getPermission())) {
-                    commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&',
-                            getPermissionMessage()));
-                    return false;
-                }
-                subCommand.execute(commandSender, args.length == 1 ? new String[0]
-                        : Arrays.copyOfRange(args, 1, args.length));
-                return false;
-            }
-        }
-        return executeCommand(commandSender, label, args);
-    }
+		if (args.length > 0) {
+			Optional<ApexSubCommand> optionalSubCommand = this.getSubCommand(args[0]);
+			if (optionalSubCommand.isPresent()) {
+				ApexSubCommand subCommand = optionalSubCommand.get();
+				if (!commandSender.hasPermission(subCommand.getPermission())) {
+					commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', getPermissionMessage()));
+					return false;
+				}
+				subCommand.execute(commandSender,
+						args.length == 1 ? new String[0] : Arrays.copyOfRange(args, 1, args.length));
+				return false;
+			}
+		}
+		return executeCommand(commandSender, label, args);
+	}
 
-    public abstract boolean executeCommand(CommandSender commandSender, String label,
-            String[] args);
+	public abstract boolean executeCommand(CommandSender commandSender, String label, String[] args);
 
-    private Optional<ApexSubCommand> getSubCommand(String key) {
-        Optional<ApexSubCommand> optionalKey = Optional.ofNullable(getSubCommandCache().get(key));
-        if (!optionalKey.isPresent()) {
-            return getSubCommandCache().values().stream()
-                    .filter(value -> Arrays.stream(value.getAliases())
-                            .anyMatch(s -> s.equalsIgnoreCase(key))).findFirst().map(Optional::of)
-                    .orElse(optionalKey);
-        }
-        return optionalKey;
-    }
+	private Optional<ApexSubCommand> getSubCommand(String key) {
+		key = key.toLowerCase();
+		Optional<ApexSubCommand> optionalKey = Optional.ofNullable(getSubCommandCache().get(key));
+		if (!optionalKey.isPresent()) {
+			return getSubCommandCache().values().stream()
+					.filter(value -> Arrays.stream(value.getAliases()).anyMatch(s -> s.equalsIgnoreCase(key)))
+					.findFirst().map(Optional::of).orElse(optionalKey);
+		}
+		return optionalKey;
+	}
 
-    public void registerSubCommand(ApexSubCommand... apexSubCommands) {
-        for (ApexSubCommand apexSubCommand : apexSubCommands) {
-            getSubCommandCache().put(apexSubCommand.getName().toLowerCase(), apexSubCommand);
-        }
-    }
+	public void registerSubCommand(ApexSubCommand... apexSubCommands) {
+		for (ApexSubCommand apexSubCommand : apexSubCommands) {
+			getSubCommandCache().put(apexSubCommand.getName().toLowerCase(), apexSubCommand);
+		}
+	}
 
-    public boolean isSubCommand(String key) {
-        return getSubCommandCache().containsKey(key);
-    }
+	public boolean isSubCommand(String key) {
+		return getSubCommandCache().containsKey(key);
+	}
 
-    public Map<String, ApexSubCommand> getSubCommandCache() {
-        return this.subCommandCache;
-    }
+	public Map<String, ApexSubCommand> getSubCommandCache() {
+		return this.subCommandCache;
+	}
 }
